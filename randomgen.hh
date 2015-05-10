254d75b57625d70da3de0f2a3f796371acd75ad6
Sergey Astanin <s.astanin@gmail.com>
2015-05-10 22:22:14 +0200

    fix random_device creation (make it work on MinGW)

--- randomgen.hh
+++ randomgen.hh
@@ -12,7 +12,7 @@ using rng_type = std::mt19937;
 
 
 std::unique_ptr<rng_type> seed_rng() {
-    std::random_device rd("/dev/urandom");
+    std::random_device rd;
     std::array<uint32_t, std::mt19937::state_size> seed_data;
     generate(seed_data.begin(), seed_data.end(), ref(rd));
     std::seed_seq sseq(std::begin(seed_data), std::end(seed_data));
