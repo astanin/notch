fbeb1dfdcb30842670d4614d62683e24219a62a5
Sergey Astanin <s.astanin@gmail.com>
2015-05-18 15:19:15 +0200

    refactor demo_perceptron

--- demo_perceptron.cpp
+++ demo_perceptron.cpp
@@ -11,62 +11,38 @@ using namespace std;
 int N_ITERS = 100;
 
 
+void print_stats(LinearPerceptron &p, const LabeledDataset &testSet) {
+    LinearPerceptronClassifier lpc(p);
+    ConfusionMatrix cm = lpc.test(testSet);
+    cout << "Synaptic weights:\n";
+    cout << "  " << p << "\n";
+    cout << "Confusion matrix:\n";
+    cout << "  TP=" << cm.truePositives << " FP=" << cm.falsePositives << "\n";
+    cout << "  FN=" << cm.falseNegatives << " TN=" << cm.trueNegatives << "\n";
+    cout << "Accuracy:\n  " << cm.accuracy() << "\n";
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         cerr << "Usage: demo_perceptron train.data test.data\n";
         exit(-1);
     }
 
-    ifstream trainIn(argv[1]);
-    ifstream testIn(argv[2]);
-    LabeledDataset trainset(FANNReader::read(trainIn));
-    LabeledDataset testset(FANNReader::read(testIn));
+    LabeledDataset trainSet(FANNReader::read(argv[1]));
+    LabeledDataset testSet(FANNReader::read(argv[2]));
 
-    LinearPerceptron p(trainset.inputDim());
-    LinearPerceptronClassifier lpc(p);
-    cout << "Initial weights:                     " << p << "\n";
-    ConfusionMatrix cm = lpc.test(testset);
-    cout << "Confusion matrix:\n";
-    cout << "    TP=" << cm.truePositives << " FP=" << cm.falsePositives
-         << "\n";
-    cout << "    FN=" << cm.falseNegatives << " TN=" << cm.trueNegatives
-         << "\n";
-    cout << "Initial accuracy:                    " << cm.accuracy() << "\n";
+    LinearPerceptron p(trainSet.inputDim());
 
-    cout << "\n1 iteration...\n\n";
-    trainConverge(p, trainset, 1, 0.1);
-    cout << "Weights after convergence training:  " << p << "\n";
-    cm = lpc.test(testset);
-    cout << "Confusion matrix:\n";
-    cout << "    TP=" << cm.truePositives << " FP=" << cm.falsePositives
-         << "\n";
-    cout << "    FN=" << cm.falseNegatives << " TN=" << cm.trueNegatives
-         << "\n";
-    cout << "Accuracy after convergence training: " << cm.accuracy() << "\n";
+    cout << "Start:\n\n";
+    print_stats(p, testSet);
 
-    cout << "\n" << N_ITERS - 1 << " more iterations...\n\n";
-    trainConverge(p, trainset, N_ITERS - 1, 0.1);
+    cout << "\nAfter " << N_ITERS << " iterations of convergence training:\n\n";
+    trainConverge(p, trainSet, N_ITERS, 0.1);
+    print_stats(p, testSet);
 
-    cout << "Weights after convergence training:  " << p << "\n";
-    cm = lpc.test(testset);
-    cout << "Confusion matrix:\n";
-    cout << "    TP=" << cm.truePositives << " FP=" << cm.falsePositives
-         << "\n";
-    cout << "    FN=" << cm.falseNegatives << " TN=" << cm.trueNegatives
-         << "\n";
-    cout << "Accuracy after convergence training: " << cm.accuracy() << "\n";
-
-    cout << "\nvs " << N_ITERS << " iterations of batch training...\n\n";
-    LinearPerceptron p2(trainset.inputDim());
-    LinearPerceptronClassifier lpc2(p2);
-    trainBatch(p2, trainset, N_ITERS, 0.1);
-    cout << "Weights after batch training:        " << p2 << "\n";
-    cm = lpc2.test(testset);
-    cout << "Confusion matrix:\n";
-    cout << "    TP=" << cm.truePositives << " FP=" << cm.falsePositives
-         << "\n";
-    cout << "    FN=" << cm.falseNegatives << " TN=" << cm.trueNegatives
-         << "\n";
-    cout << "Accuracy after batch training:       " << cm.accuracy() << "\n";
+    LinearPerceptron p2(trainSet.inputDim());
+    cout << "\nvs after " << N_ITERS << " iterations of batch training...\n\n";
+    trainBatch(p2, trainSet, N_ITERS, 0.1);
+    print_stats(p2, testSet);
 }
 
