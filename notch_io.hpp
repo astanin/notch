e1c44383064e8806aef376bfd80b64697abdc293
Sergey Astanin <s.astanin@gmail.com>
2015-05-20 13:20:43 +0200

    CSVReader with categorical data conversion

--- notch_io.hpp
+++ notch_io.hpp
@@ -29,10 +29,13 @@ THE SOFTWARE.
 
 */
 
+#include <algorithm>  // find_if
 #include <fstream>    // ifstream
 #include <istream>
+#include <map>
 #include <ostream>
 #include <sstream>    // ostringstream
+#include <stdexcept>  // out_of_range
 
 #include "notch.hpp"
 
@@ -93,6 +96,222 @@ public:
             Output output(outputDimension);
             in >> input >> output;
             ds.append(input, output);
+        }
+        return ds;
+    }
+};
+
+/** Load labeled datasets from CSV files.
+ *
+ * Numeric columns are converted to `float` numbers.
+ *
+ * Values of non-numeric columns are converted to numbers in range [0;N-1],
+ * where N is the number of unique values in the column.
+ *
+ * By default the last column is used as a label (`labelcols={-1}`).
+ **/
+template<char delimiter=','>
+class CSVReader {
+private:
+    enum class CellTag { Number, String };
+
+    struct Cell {
+        CellTag tag;
+        float value;
+        std::string str;
+    };
+
+    using TextTable = std::vector<std::vector<std::string>>;
+    using MixedTable = std::vector<std::vector<Cell>>;
+    using MixedRow = std::vector<Cell>;
+    using NumericTable = std::vector<std::vector<float>>;
+    using NumericRow = std::vector<float>;
+
+    enum class CSVReaderState {
+        UnquotedField,
+        QuotedField,
+        QuotedQuote
+    };
+
+    /// Parse a row of an Excel CSV file.
+    static std::vector<std::string>
+    readCSVRow(const std::string &row) {
+        CSVReaderState state = CSVReaderState::UnquotedField;
+        std::vector<std::string> fields {""};
+        size_t i = 0; // index of the current field
+        for (char c : row) {
+            switch (state) {
+                case CSVReaderState::UnquotedField:
+                    switch (c) {
+                        case delimiter: // end of field
+                                  fields.push_back(""); i++;
+                                  break;
+                        case '"': state = CSVReaderState::QuotedField;
+                                  break;
+                        default:  fields[i].push_back(c);
+                                  break; }
+                    break;
+                case CSVReaderState::QuotedField:
+                    switch (c) {
+                        case '"': state = CSVReaderState::QuotedQuote;
+                                  break;
+                        default:  fields[i].push_back(c);
+                                  break; }
+                    break;
+                case CSVReaderState::QuotedQuote:
+                    switch (c) {
+                        case delimiter: // , after closing quote
+                                  fields.push_back(""); i++;
+                                  state = CSVReaderState::UnquotedField;
+                                  break;
+                        case '"': // "" -> "
+                                  fields[i].push_back('"');
+                                  state = CSVReaderState::QuotedField;
+                                  break;
+                        default:  // end of quote
+                                  state = CSVReaderState::UnquotedField;
+                                  break; }
+                    break;
+            }
+        }
+        return fields;
+    }
+
+    /// Read Excel CSV file. Skip empty rows.
+    static TextTable
+    readCSV(std::istream &in, int skiprows=0, int skipcols=0) {
+        TextTable table;
+        std::string row;
+        while (true) {
+            std::getline(in, row);
+            if (in.bad() || in.eof()) {
+                break;
+            }
+            if (skiprows > 0) {
+                --skiprows;
+                continue;
+            }
+            if (row.size() == 0) {
+                continue;
+            }
+            auto fields = readCSVRow(row);
+            if (skipcols > 0) {
+                auto field0 = std::begin(fields);
+                auto fieldToKeep = field0 + skipcols;
+                fields.erase(field0, fieldToKeep);
+            }
+            table.push_back(fields);
+        }
+        return table;
+    }
+
+    static Cell
+    parseCell(const std::string &s) {
+        // float value = std::stof(s); // doesn't work on MinGW-32; BUG #52015
+        const char *parseBegin = s.c_str();
+        char *parseEnd = nullptr;
+        float value = strtof(parseBegin, &parseEnd);
+        if (parseEnd == parseBegin || parseEnd == nullptr) {
+            return Cell { CellTag::String, 0.0, s };
+        } else {
+            return Cell { CellTag::Number, value, "" };
+        }
+    }
+
+    static MixedTable
+    parseTable(const TextTable &table) {
+       MixedTable cellTable(0);
+       for (auto row : table) {
+           MixedRow cellRow(0);
+           for (auto s : row) {
+               cellRow.push_back(parseCell(s));
+           }
+           cellTable.push_back(cellRow);
+       }
+       return cellTable;
+    }
+
+    static bool
+    isColumnNumeric(const MixedTable &t, size_t column_idx) throw (std::out_of_range) {
+        return std::all_of(std::begin(t), std::end(t),
+                [column_idx](const MixedRow &r) {
+                    return r.at(column_idx).tag == CellTag::Number;
+                });
+    }
+
+    ///
+    static NumericTable
+    convertToNumeric(const MixedTable &t) throw (std::out_of_range) {
+        // analyze table
+        size_t ncols = t.at(0).size();
+        std::vector<bool> isNumeric(ncols);
+        for (size_t i = 0; i < ncols; ++i) {
+            isNumeric[i] = isColumnNumeric(t, i);
+        }
+        // convert table
+        std::vector<std::map<std::string, int>> columnKeys(ncols);
+        NumericTable nt;
+        for (auto row : t) {
+            NumericRow nr;
+            for (size_t i = 0; i < ncols; ++i) {
+                if (isNumeric[i]) {
+                    nr.push_back(row[i].value);
+                } else { // a non-numeric label
+                    auto label = row[i].str;
+                    if (columnKeys[i].count(label)) { // a previosly seen label
+                        int labelIndex = columnKeys[i][label];
+                        nr.push_back(labelIndex);
+                    } else { // new label
+                        int labelIndex = columnKeys[i].size();
+                        columnKeys[i][label] = labelIndex;
+                        nr.push_back(labelIndex);
+                    }
+                }
+            }
+            nt.push_back(nr);
+        }
+        return nt;
+    }
+
+public:
+    static LabeledDataset
+    read(const std::string &path, std::vector<int> labelcols = {-1},
+         int skiprows=0, int skipcols=0) {
+        std::ifstream in(path);
+        return CSVReader::read(in, skiprows, skipcols);
+    }
+
+    static LabeledDataset
+    read(std::istream &in, std::vector<int> labelcols = {-1},
+         int skiprows=0, int skipcols=0) {
+        LabeledDataset ds;
+        auto rows = readCSV(in, skiprows, skipcols);
+        auto mixedRows = parseTable(rows);
+        auto numericRows = convertToNumeric(mixedRows);
+        for (auto row : numericRows) {
+            // build label vector
+            Output label(labelcols.size());
+            size_t ncols = row.size();
+            for (size_t i = 0; i < labelcols.size(); ++i) {
+                size_t colIdx = ((ncols + labelcols[i]) % ncols);
+                label[i] = row[colIdx];
+            }
+            // remove label columns from the row (from the end)
+            for (int i = row.size()-1; i >= 0; --i) {
+                auto found = std::find_if(labelcols.begin(), labelcols.end(),
+                             [=](int labelcol) {
+                                return (labelcol + ncols) % ncols == i;
+                             });
+                if (found != labelcols.end()) {
+                    row.erase(row.begin() + i);
+                }
+            }
+            // build data vector
+            Input data(row.size());
+            for (size_t i = 0; i < row.size(); ++i) {
+                data[i] = row[i];
+            }
+            ds.append(data, label);
         }
         return ds;
     }
