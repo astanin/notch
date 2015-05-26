0b664ec414083930afdf06cee0fdcc7a635cda84
Sergey Astanin <s.astanin@gmail.com>
2015-05-26 17:07:24 +0200

    tests for CSVReader

--- test/test_notch_io.hpp
+++ test/test_notch_io.hpp
@@ -30,22 +30,18 @@ TEST_CASE("Array plain-text I/O", "[io]") {
 TEST_CASE("Dataset FANN-format reader", "[io]") {
     stringstream ss("4 2 1\n0.0 0\n0\n0.0 1.0\n1.0\n1.0 0.0\n1\n1 1\n0.0");
     LabeledDataset d = FANNReader::read(ss);
-    CHECK(d.size() == 4);
-    CHECK(d.inputDim() == 2);
-    CHECK(d.outputDim() == 1);
-    LabeledDataset good {{{0,0},{0}},
-                         {{0,1},{1}},
-                         {{1,0},{1}},
-                         {{1,1},{0}}};
+    LabeledDataset expected {{{0,0},{0}}, {{0,1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    CHECK(d.size() == expected.size());
+    CHECK(d.inputDim() == expected.inputDim());
+    CHECK(d.outputDim() == expected.outputDim());
     auto d_it = begin(d);
-    auto d_end = end(d);
-    auto good_it = begin(good);
-    auto good_end = end(good);
+    auto good_it = begin(expected);
+    auto good_end = end(expected);
     for (; good_it != good_end; ++d_it, ++good_it) {
-        for (size_t i = 0; i < good.inputDim(); ++i) {
+        for (size_t i = 0; i < expected.inputDim(); ++i) {
             CHECK((*good_it).data[i] == (*d_it).data[i]);
         }
-        for (size_t i = 0; i < good.outputDim(); ++i) {
+        for (size_t i = 0; i < expected.outputDim(); ++i) {
             CHECK((*good_it).label[i] == (*d_it).label[i]);
         }
     }
@@ -56,6 +52,69 @@ TEST_CASE("Dataset FANN-format writer", "[io]") {
     ostringstream ss;
     ss << FANNFormat(xorD);
     CHECK(ss.str() == "4 2 1\n0 0\n0\n0 1\n1\n1 0\n1\n1 1\n0\n");
+}
+
+TEST_CASE("Dataset CSV-format reader", "[io]") {
+    stringstream csv("0.0,0.0,0.0\n0,1,1\n1,0,1\n1,1,0\n");
+    LabeledDataset d = CSVReader<','>::read(csv);
+    LabeledDataset expected {{{0,0},{0}}, {{0,1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    // compare two datasets
+    CHECK(d.size() == expected.size());
+    CHECK(d.inputDim() == expected.inputDim());
+    CHECK(d.outputDim() == expected.outputDim());
+    auto d_it = begin(d);
+    auto good_it = begin(expected);
+    auto good_end = end(expected);
+    for (; good_it != good_end; ++d_it, ++good_it) {
+        for (size_t i = 0; i < expected.inputDim(); ++i) {
+            CHECK((*good_it).data[i] == (*d_it).data[i]);
+        }
+        for (size_t i = 0; i < expected.outputDim(); ++i) {
+            CHECK((*good_it).label[i] == (*d_it).label[i]);
+        }
+    }
+}
+
+TEST_CASE("Dataset CSV-format reader (skiprows)", "[io]") {
+    stringstream csv("x,y,XOR\n0.0,0.0,0.0\n0,1,1\n1,0,1\n1,1,0\n");
+    LabeledDataset d = CSVReader<','>::read(csv, {2} /* last col */, 1 /* skiprows */);
+    LabeledDataset expected {{{0,0},{0}}, {{0,1},{1}}, {{1,0},{1}}, {{1,1},{0}}};
+    // compare two datasets
+    CHECK(d.size() == expected.size());
+    CHECK(d.inputDim() == expected.inputDim());
+    CHECK(d.outputDim() == expected.outputDim());
+    auto d_it = begin(d);
+    auto good_it = begin(expected);
+    auto good_end = end(expected);
+    for (; good_it != good_end; ++d_it, ++good_it) {
+        for (size_t i = 0; i < expected.inputDim(); ++i) {
+            CHECK((*good_it).data[i] == (*d_it).data[i]);
+        }
+        for (size_t i = 0; i < expected.outputDim(); ++i) {
+            CHECK((*good_it).label[i] == (*d_it).label[i]);
+        }
+    }
+}
+
+TEST_CASE("Dataset CSV-format reader (categorical labels and quotes)", "[io]") {
+    stringstream csv("1,odd\n\"2\",even\n3,odd\n4,\"\"\"four\"\"\"\n");
+    LabeledDataset d = CSVReader<>::read(csv);
+    LabeledDataset expected {{{1},{0}}, {{2},{1}}, {{3},{0}}, {{4},{2}}};
+    // compare two datasets
+    CHECK(d.size() == expected.size());
+    CHECK(d.inputDim() == expected.inputDim());
+    CHECK(d.outputDim() == expected.outputDim());
+    auto d_it = begin(d);
+    auto good_it = begin(expected);
+    auto good_end = end(expected);
+    for (; good_it != good_end; ++d_it, ++good_it) {
+        for (size_t i = 0; i < expected.inputDim(); ++i) {
+            CHECK((*good_it).data[i] == (*d_it).data[i]);
+        }
+        for (size_t i = 0; i < expected.outputDim(); ++i) {
+            CHECK((*good_it).label[i] == (*d_it).label[i]);
+        }
+    }
 }
 
 #endif
