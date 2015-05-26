d2146d89534949355481c0ed563d24a6ce03e1f2
Sergey Astanin <s.astanin@gmail.com>
2015-05-27 01:50:01 +0200

    simplify demo_multilayer_xor

--- examples/demo_multilayer_xor.cpp
+++ examples/demo_multilayer_xor.cpp
@@ -12,39 +12,36 @@
 using namespace std;
 
 
-int main(int, char *[]) {
-    unique_ptr<RNG> rng(newRNG());
-    LabeledDataset trainSet {{{0,0},{0}},
-                         {{0,1},{1}},
-                         {{1,0},{1}},
-                         {{1,1},{0}}};
-    LabeledDataset &testSet(trainSet);
-    cout << "training set:\n" << CSVFormat(trainSet) << "\n";
-    MultilayerPerceptron xorNet({2, 2, 1}, scaledTanh);
-    xorNet.init(rng);
-    cout << "initial NN:\n\n";
-    PlainTextNetworkWriter(cout) << xorNet;
-    cout << "initial out:\n";
-    for (auto s : trainSet) {
+void print_net(string tag, MultilayerPerceptron &xorNet, LabeledDataset dataset) {
+    PlainTextNetworkWriter(cout) << tag << " net:\n\n" << xorNet << "\n";
+    cout << tag << " out:\n";
+    for (auto s : dataset) {
         cout << s.data << " -> " << *xorNet.output(s.data) << "\n";
     }
     cout << "\n";
+}
+
+
+int main(int, char *[]) {
+    unique_ptr<RNG> rng(newRNG());
+    LabeledDataset dataset {{{0,0},{0}},
+                            {{0,1},{1}},
+                            {{1,0},{1}},
+                            {{1,1},{0}}};
+    MultilayerPerceptron xorNet({2, 2, 1}, scaledTanh);
+    xorNet.init(rng);
+
+    cout << "training set:\n" << CSVFormat(dataset) << "\n";
+    print_net("initial", xorNet, dataset);
 
     xorNet.setLearningPolicy(0.01f, 0.9);
-    trainWithSGD(xorNet, trainSet, rng, /* epochs */ 500,
-                 /* callbackEvery */ 25,
+    trainWithSGD(xorNet, dataset, rng, /* epochs */ 500,
+                 /* callbackEvery */ 100,
                  /* callback */ [&](int i, ABackpropLayer& net) {
-                     printLoss(i, net, testSet);
+                     printLoss(i, net, dataset);
                 });
     cout << "\n";
 
-    cout << "final NN:\n\n";
-    PlainTextNetworkWriter(cout) << xorNet;
-    cout << "final out:\n";
-    for (auto s : trainSet) {
-        cout << s.data << " -> " << *xorNet.output(s.data) << "\n";
-    }
-    cout << "\n";
-
+    print_net("final", xorNet, dataset);
     return 0;
 }
