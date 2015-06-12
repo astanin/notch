8b76c72c97f0534df802c8ebcc9040922d614650
Sergey Astanin <s.astanin@gmail.com>
2015-06-12 14:21:41 +0200

    more tests for OneHotEncoder: two columns and selected column

--- test/test_notch_pre.cpp
+++ test/test_notch_pre.cpp
@@ -14,21 +14,17 @@ TEST_CASE("OneHotEncoder single-column encoding-decoding", "[pre]") {
     OneHotEncoder enc {five};
     Dataset fiveEncoded = enc.apply(five);
     // check expected encoding:
-    // 0 0 0 0 1
-    // 1 0 0 0 0
-    // 0 0 0 1 0
-    // 0 0 1 0 0
-    // 0 1 0 0 0
+    Dataset expected {{0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0},
+                      {0, 0, 0, 1, 0},
+                      {0, 0, 1, 0, 0},
+                      {0, 1, 0, 0, 0}};
     for (size_t i = 0; i < fiveEncoded.size(); ++i) {
         CHECK(fiveEncoded[i].size() == 5);
         float sum = 0;
         for(size_t j = 0; j < fiveEncoded[i].size(); ++j) {
             sum += fiveEncoded[i][j];
-            if (five[i][0]-1 == j) { // labels start at 1
-                CHECK(fiveEncoded[i][j] == 1.0);
-            } else {
-                CHECK(fiveEncoded[i][j] == 0.0);
-            }
+            CHECK(fiveEncoded[i][j] == expected[i][j]);
         }
         CHECK(sum == 1.0);
     }
@@ -38,6 +34,67 @@ TEST_CASE("OneHotEncoder single-column encoding-decoding", "[pre]") {
         CHECK(x.size() == five[i].size());
         for (size_t j = 0; j < x.size(); ++j) {
             CHECK(x[j] == five[i][j]);
+        }
+    }
+}
+
+TEST_CASE("OneHotEncoder two-column encoding-decoding", "[pre]") {
+    Dataset twocols {{1,10},{3,20},{2,20},{4,30}}; // 4 and 3 distinct values
+    OneHotEncoder enc(twocols);
+    Dataset encoded = enc.apply(twocols);
+    // check expected encoding:
+    Dataset expected {{1, 0, 0, 0, 1, 0, 0},
+                      {0, 0, 1, 0, 0, 1, 0},
+                      {0, 1, 0, 0, 0, 1, 0},
+                      {0, 0, 0, 1, 0, 0, 1}};
+    size_t col1_code_size = 4;
+    size_t col2_code_size = 3;
+    for (size_t i = 0; i < encoded.size(); ++i) {
+        CHECK(encoded[i].size() == (col1_code_size + col2_code_size));
+        float sum = 0;
+        for(size_t j = 0; j < encoded[i].size(); ++j) {
+            sum += encoded[i][j];
+            CHECK(encoded[i][j] == expected[i][j]);
+        }
+        CHECK(sum == 2.0); // 2 columns
+    }
+    // check decoding
+    for (size_t i = 0; i < encoded.size(); ++i) {
+        auto x = enc.unapply(encoded[i]);
+        CHECK(x.size() == twocols[i].size());
+        for (size_t j = 0; j < x.size(); ++j) {
+            CHECK(x[j] == twocols[i][j]);
+        }
+    }
+}
+
+TEST_CASE("OneHotEncoder column selection (negative index)", "[pre]") {
+    Dataset twocols {{1,10},{3,20},{2,20},{4,30}}; // 4 and 3 distinct values
+    OneHotEncoder enc(twocols, {-2}); // the first (the last but one) column
+    Dataset encoded = enc.apply(twocols);
+    // check expected encoding:
+    Dataset expected {{1, 0, 0, 0, 10},
+                      {0, 0, 1, 0, 20},
+                      {0, 1, 0, 0, 20},
+                      {0, 0, 0, 1, 30}};
+    size_t col1_code_size = 4;
+    for (size_t i = 0; i < encoded.size(); ++i) {
+        CHECK(encoded[i].size() == col1_code_size + 1);
+        float sum = 0;
+        for(size_t j = 0; j < encoded[i].size(); ++j) {
+            CHECK(encoded[i][j] == expected[i][j]);
+            if (j < col1_code_size) {
+                sum += encoded[i][j];
+            }
+        }
+        CHECK(sum == 1.0); // 1 column
+    }
+    // check decoding
+    for (size_t i = 0; i < encoded.size(); ++i) {
+        auto x = enc.unapply(encoded[i]);
+        CHECK(x.size() == twocols[i].size());
+        for (size_t j = 0; j < x.size(); ++j) {
+            CHECK(x[j] == twocols[i][j]);
         }
     }
 }
