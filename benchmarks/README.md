9f50f114faaddf5eca6355306f7f75b84307a682
Sergey Astanin <s.astanin@gmail.com>
2015-07-07 18:17:32 +0200

    update benchmarks script and re-run it in Linux

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -1,32 +1,44 @@
-Benchmarks: Notch vs Torch7
-============================
+Notch vs Torch7
+===============
 
-Benchmark date:   2015-07-01T15:38+0000
+Until Notch gets more features and more rigorous benchmarks are written, 
+this is a quick-and-dirty comparison of Notch (this library) and Torch7
+(a popular and one of the best performing deep learning frameworks).
+
+The libraries are compared in CPU mode. Torch7 is installed using 
+ezinstall script from http://torch.ch/docs/getting-started.html
+Notch implementation logs the same amount of information as the Torch7 script.
+Output is redirected to files.
+
+Notch version:    b360d99a
+
+Torch7 version:   b30f4bd3
+
+
+Benchmark date:   2015-07-07T16:12+0000
 
 Benchmark CPU:    Intel(R) Core(TM) i7-2600 CPU @ 3.40GHz
 
-Benchmark OS:     Linux 3.16.0-30-generic
+Benchmark OS:     Ubuntu 14.04.2 LTS, Trusty Tahr ;  Linux x86_64
 
 C++ compiler:     g++ (Ubuntu 4.8.4-2ubuntu1~14.04) 4.8.4
 
-BLAS:             libopenblas.so
+BLAS:             libopenblas_sandybridgep-r0.2.14.so
 
 
 Benchmarks
 ----------
 
- * twospirals: a 2-50-50-50-50-10-1 multilayer perceptron
+### Two Spirals
+
+Run 1000 epochs of training a 2-50-50-50-50-10-1 multilayer perceptron.
 
 
-Log
----
-
-~~~
-Torch7 twospirals SGD 1000 iters: OK # training error = 0.99146835152189
-Torch7 twospirals SGD real time:  107.23
-Notch twospirals SGD 1000 iters: OK # training error = 0.923686
-Notch twospirals SGD real time:  121.75
-Notch twospirals SGD+ADADELTA 1000 iters: OK # training error = 0.973669
-Notch twospirals SGD+ADADELTA real time:  115.00
-~~~
+|library and task                                  |   time, s| samples/s|
+|:-------------------------------------------------|---------:|---------:|
+|Torch7 SGD FixedRate                              |     23.37|      8258|
+|Notch SGD FixedRate                               |     11.70|     16496|
+|Notch SGD ADADELTA                                |     11.12|     17356|
+|Notch (OpenMP, no BLAS) SGD FixedRate             |     15.17|     12722|
+|Notch (no OpenMP, no BLAS) SGD FixedRate          |     11.05|     17466|
 
--- benchmarks/runbenchmarks.sh
+++ benchmarks/runbenchmarks.sh
@@ -2,18 +2,40 @@
 
 BENCHMARK_DATE=$(date --iso-8601=minutes -u)
 CPU_MODEL=$(cat /proc/cpuinfo | grep '^model name' | head -1 | sed 's/[^:]*: //')
-BLAS_LIBRARY=$(ls /opt/OpenBLAS/lib/libopenblas*[0-9]*.so | head -1 | sed 's/.*\///')
+OS_INFO=$(. /etc/os-release && echo $NAME $VERSION '; ' $(uname -sm))
+BLAS_LIBRARY=$(ls -S /opt/OpenBLAS/lib/libopenblas*[0-9]*.so | head -1 | sed 's/.*\///')
+
+
+NOTCH_VERSION=$(git rev-parse HEAD|fold -8|head -1)
+TORCH7_VERSION=$((cd ~/torch/pkg/torch && git rev-parse HEAD)|fold -8|head -1)
+
+
 N_ITERS=1000
 N_SAMPLES=193  # twospirals-train
 
 
+# TODO: write Notch benchmarks for https://github.com/soumith/convnet-benchmarks
+
 echo "Notch vs Torch7"
 echo "==============="
+echo
+echo "Until Notch gets more features and more rigorous benchmarks are written, "
+echo "this is a quick-and-dirty comparison of Notch (this library) and Torch7"
+echo "(a popular and one of the best performing deep learning frameworks)."
+echo
+echo "The libraries are compared in CPU mode. Torch7 is installed using "
+echo "ezinstall script from http://torch.ch/docs/getting-started.html"
+echo "Notch implementation logs the same amount of information as the Torch7 script."
+echo "Output is redirected to files."
+echo
+
+echo "Notch version:    $NOTCH_VERSION" ; echo
+echo "Torch7 version:   $TORCH7_VERSION" ; echo
 echo
 
 echo "Benchmark date:   $BENCHMARK_DATE" ; echo
 echo "Benchmark CPU:    $CPU_MODEL" ; echo
-echo "Benchmark OS:     $(uname -sr)" ; echo
+echo "Benchmark OS:     $OS_INFO" ; echo
 echo "C++ compiler:     $(g++ --version | head -1)" ; echo
 echo "BLAS:             ${BLAS_LIBRARY}" ; echo
 echo
@@ -21,17 +43,19 @@ echo
 echo "Benchmarks"
 echo "----------"
 echo
-echo " * twospirals: a 2-50-50-50-50-10-1 multilayer perceptron"
+echo "### Two Spirals"
 echo
+echo "Run $N_ITERS epochs of training a 2-50-50-50-50-10-1 multilayer perceptron."
 echo
 
 rm *.err *.log
 RESULTS=./benchmark_results.txt
 test -f $RESULTS && rm $RESULTS
->$RESULTS  printf "|%-30s|%10s|%10s|\n" "library and task" "time, s" "samples/s"
->>$RESULTS printf "|:-----------------------------|---------:|---------:|\n"
+>$RESULTS  printf "|%-50s|%10s|%10s|\n" "library and task" "time, s" "samples/s"
+>>$RESULTS printf "|:-------------------------------------------------|---------:|---------:|\n"
 
 export LC_NUMERIC=C
+export LD_LIBRARY_PATH=/opt/OpenBLAS/lib
 
 (
     # requires torch7 (th) to be installed
@@ -39,44 +63,46 @@ export LC_NUMERIC=C
     HAS_TORCH=$?
 
     if [[ $HAS_TORCH -eq 0 ]]; then
-        echo -n "Torch SGD $N_ITERS iters... " ;
+        echo -n "Torch SGD..." ;
         time -p ( th bench_twospirals.lua $N_ITERS > torch.log ) 2> torch.err \
             && ( echo OK ; \
                  TORCH_TIME=$(awk '/^real /{print $2;}' torch.err) ; \
                  TORCH_SPS=$(awk "BEGIN {print $N_ITERS * $N_SAMPLES / $TORCH_TIME;}") ; \
-                 >> $RESULTS printf "|%-30s|%10.2f|%10.0f|\n" \
-                             "Torch7 $N_ITERS SGD iters" $TORCH_TIME $TORCH_SPS) \
+                 >> $RESULTS printf "|%-50s|%10.2f|%10.0f|\n" \
+                             "Torch7 SGD FixedRate" $TORCH_TIME $TORCH_SPS) \
             || ( echo "FAILED # see torch.err" )
     fi
 ) >&2
 
 (
     # requires GNU C++ compiler and OpenBLAS in /opt/OpenBLAS
-    test -f ./bench_twospirals && rm ./bench_twospirals
-    g++ -std=c++11 -O3 -I.. -DNOTCH_USE_CBLAS bench_twospirals.cpp \
-        -L/opt/OpenBLAS/lib -lopenblas -o bench_twospirals \
+    test -f ./bench_twospirals_blas && rm ./bench_twospirals_blas
+    g++ -std=c++11 -O3 -I.. -DNOTCH_USE_CBLAS -I/opt/OpenBLAS/include \
+        bench_twospirals.cpp \
+        -L/opt/OpenBLAS/lib -lopenblas \
+        -o bench_twospirals_blas \
         || (echo "Notch build with BLAS FAILED")
-    HAS_NOTCH=$(test -f ./bench_twospirals)
+    HAS_NOTCH=$(test -f ./bench_twospirals_blas)
 
     if [[ $HAS_NOTCH -eq 0 ]]; then
-        echo -n "Notch SGD $N_ITERS iters... " ;
-        time -p ( ./bench_twospirals $N_ITERS > notch_sgd.log ) 2> notch_sgd.err \
+        echo -n "Notch SGD... " ;
+        time -p ( ./bench_twospirals_blas $N_ITERS > notch_sgd.log ) 2> notch_sgd.err \
             && ( echo OK ; \
                  EXE_TIME=$(awk '/^real /{print $2;}' notch_sgd.err) ; \
                  EXE_SPS=$(awk "BEGIN {print $N_ITERS * $N_SAMPLES / $EXE_TIME;}") ; \
-                 >> $RESULTS printf "|%-30s|%10.2f|%10.0f|\n" \
-                             "Notch $N_ITERS SGD iters" $EXE_TIME $EXE_SPS) \
+                 >> $RESULTS printf "|%-50s|%10.2f|%10.0f|\n" \
+                             "Notch SGD FixedRate" $EXE_TIME $EXE_SPS) \
             || ( echo "FAILED # see notch_sgd.err" )
     fi
 
     if [[ $HAS_NOTCH -eq 0 ]]; then
-        echo -n "Notch ADADELTA $N_ITERS iters... " ;
-        time -p ( ./bench_twospirals $N_ITERS --adadelta > notch_adadelta.log ) 2> notch_adadelta.err  \
+        echo -n "Notch ADADELTA... " ;
+        time -p ( ./bench_twospirals_blas $N_ITERS --adadelta > notch_adadelta.log ) 2> notch_adadelta.err  \
             && ( echo OK ; \
                  EXE_TIME=$(awk '/^real /{print $2;}' notch_adadelta.err ) ; \
                  EXE_SPS=$(awk "BEGIN {print $N_ITERS * $N_SAMPLES / $EXE_TIME;}") ; \
-                 >> $RESULTS printf "|%-30s|%10.2f|%10.0f|\n" \
-                             "Notch $N_ITERS ADADELTA iters" $EXE_TIME $EXE_SPS) \
+                 >> $RESULTS printf "|%-50s|%10.2f|%10.0f|\n" \
+                             "Notch SGD ADADELTA" $EXE_TIME $EXE_SPS) \
             || ( echo "FAILED # see notch_adadelta.err" )
     fi
 ) >&2
@@ -90,21 +116,39 @@ export LC_NUMERIC=C
     HAS_NOTCH=$(test -f ./bench_twospirals_noblas)
 
     if [[ $HAS_NOTCH -eq 0 ]]; then
-        echo -n "Notch (no BLAS) SGD $N_ITERS iters... " ;
+        echo -n "Notch SGD FixedRate (no BLAS)..." ;
         time -p ( ./bench_twospirals_noblas $N_ITERS > notch_noblas_sgd.log ) 2> notch_noblas_sgd.err \
             && ( echo OK ; \
                  EXE_TIME=$(awk '/^real /{print $2;}' notch_noblas_sgd.err) ; \
                  EXE_SPS=$(awk "BEGIN {print $N_ITERS * $N_SAMPLES / $EXE_TIME;}") ; \
-                 >> $RESULTS printf "|%-30s|%10.2f|%10.0f|\n" \
-                             "Notch (no BLAS) $N_ITERS SGD iters" $EXE_TIME $EXE_SPS) \
+                 >> $RESULTS printf "|%-50s|%10.2f|%10.0f|\n" \
+                             "Notch (OpenMP, no BLAS) SGD FixedRate" $EXE_TIME $EXE_SPS) \
             || ( echo "FAILED # see notch_noblas_sgd.err" )
     fi
 
 ) >&2
 
-echo
-echo "Results"
-echo "-------"
+(
+    # requires only GNU C++ compiler
+    test -f ./bench_twospirals_baseline && rm ./bench_twospirals_baseline
+    g++ -std=c++11 -O3 -I.. \
+        bench_twospirals.cpp -o bench_twospirals_baseline \
+        || (echo "Notch build without BLAS FAILED")
+    HAS_NOTCH=$(test -f ./bench_twospirals_baseline)
+
+    if [[ $HAS_NOTCH -eq 0 ]]; then
+        echo -n "Notch SGD FixedRate (no BLAS, no OpenMP)..." ;
+        time -p ( ./bench_twospirals_baseline $N_ITERS > notch_baseline_sgd.log ) 2> notch_baseline_sgd.err \
+            && ( echo OK ; \
+                 EXE_TIME=$(awk '/^real /{print $2;}' notch_baseline_sgd.err) ; \
+                 EXE_SPS=$(awk "BEGIN {print $N_ITERS * $N_SAMPLES / $EXE_TIME;}") ; \
+                 >> $RESULTS printf "|%-50s|%10.2f|%10.0f|\n" \
+                             "Notch (no OpenMP, no BLAS) SGD FixedRate" $EXE_TIME $EXE_SPS) \
+            || ( echo "FAILED # see notch_baseline_sgd.err" )
+    fi
+
+) >&2
+
 echo
 cat $RESULTS
 echo
